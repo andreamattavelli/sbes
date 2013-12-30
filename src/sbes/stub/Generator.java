c42fe625db074ab9f769f1e200be5e095b4c79b2
Andrea Mattavelli <andreamattavelli@gmail.com>
2013-12-30 15:32:57 +0000

    First ugly piece of prototype to generate and dump almost correct stubs
    git-svn-id: http://star.inf.usi.ch/repos/code/andrea/sbes@7360 62d94d92-d06f-44e7-bb87-548ab0999166

--- src/sbes/stub/Generator.java
+++ src/sbes/stub/Generator.java
@@ -10,27 +10,30 @@ import japa.parser.ast.body.Parameter;
 import japa.parser.ast.body.TypeDeclaration;
 import japa.parser.ast.body.VariableDeclarator;
 import japa.parser.ast.body.VariableDeclaratorId;
+import japa.parser.ast.expr.ArrayAccessExpr;
 import japa.parser.ast.expr.ArrayCreationExpr;
-import japa.parser.ast.expr.ArrayInitializerExpr;
 import japa.parser.ast.expr.AssignExpr;
 import japa.parser.ast.expr.AssignExpr.Operator;
-import japa.parser.ast.expr.ArrayAccessExpr;
 import japa.parser.ast.expr.BinaryExpr;
-import japa.parser.ast.expr.ConditionalExpr;
 import japa.parser.ast.expr.Expression;
+import japa.parser.ast.expr.FieldAccessExpr;
 import japa.parser.ast.expr.IntegerLiteralExpr;
 import japa.parser.ast.expr.MethodCallExpr;
+import japa.parser.ast.expr.StringLiteralExpr;
+import japa.parser.ast.expr.ThisExpr;
 import japa.parser.ast.expr.UnaryExpr;
 import japa.parser.ast.expr.VariableDeclarationExpr;
 import japa.parser.ast.stmt.BlockStmt;
 import japa.parser.ast.stmt.ExpressionStmt;
 import japa.parser.ast.stmt.ForStmt;
+import japa.parser.ast.stmt.IfStmt;
 import japa.parser.ast.stmt.Statement;
-import japa.parser.ast.type.PrimitiveType;
-import japa.parser.ast.type.PrimitiveType.Primitive;
 import japa.parser.ast.type.Type;
 
-import java.lang.reflect.GenericArrayType;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -117,7 +120,7 @@ public class Generator {
 		BodyDeclaration es_bd = new FieldDeclaration(Modifier.PRIVATE | Modifier.FINAL | Modifier.STATIC, ASTHelper.createReferenceType(c.getSimpleName(), 1), expected_states);
 		
 		ArrayCreationExpr er_ace = new ArrayCreationExpr(targetReturnType, dimension, 0);
-		VariableDeclarator expected_results = new VariableDeclarator(new VariableDeclaratorId(EXPECTED_STATE), er_ace);
+		VariableDeclarator expected_results = new VariableDeclarator(new VariableDeclaratorId(EXPECTED_RESULT), er_ace);
 		BodyDeclaration er_bd = new FieldDeclaration(Modifier.PRIVATE | Modifier.FINAL | Modifier.STATIC, ASTHelper.createReferenceType(targetMethod.getGenericReturnType().toString(), 1), expected_results);
 		
 		ArrayCreationExpr as_ace = new ArrayCreationExpr(classType, dimension, 0);
@@ -125,7 +128,7 @@ public class Generator {
 		BodyDeclaration as_bd = new FieldDeclaration(Modifier.PRIVATE | Modifier.FINAL | Modifier.STATIC, ASTHelper.createReferenceType(c.getSimpleName(), 1), actual_states);
 		
 		ArrayCreationExpr ar_ace = new ArrayCreationExpr(targetReturnType, dimension, 0);
-		VariableDeclarator actual_results = new VariableDeclarator(new VariableDeclaratorId(ACTUAL_STATE), ar_ace);
+		VariableDeclarator actual_results = new VariableDeclarator(new VariableDeclaratorId(ACTUAL_RESULT), ar_ace);
 		BodyDeclaration ar_bd = new FieldDeclaration(Modifier.PRIVATE | Modifier.FINAL | Modifier.STATIC, ASTHelper.createReferenceType(targetMethod.getGenericReturnType().toString(), 1), actual_results);
 		
 		List<BodyDeclaration> members = new ArrayList<BodyDeclaration>();
@@ -221,12 +224,35 @@ public class Generator {
 			members.add(md);
 		}
 		
+		// set_results artificial method
+		members.add(createSetResultsMethod(targetReturnType));
+		members.add(createMethodUnderTest(targetReturnType));
+		
 		td.setMembers(members);
 		
-		System.out.println(cu.toString());
-		
+		dumpTestCase(cu, c.getSimpleName() + "_Stub");
 	}
 	
+	private void dumpTestCase(CompilationUnit cu, String name) {
+		BufferedWriter out = null;
+		try {
+			String filename = name + ".java";
+			out = new BufferedWriter(new FileWriter(filename));
+			out.write(cu.toString());
+			out.close();
+		} catch(IOException e) {
+			logger.error("Unable to dump stub due to: " + e.getMessage());
+		} finally{
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					logger.error("Unable to correctly dump stub due to: " + e.getMessage());
+				}
+			}
+		}
+	}
+
 	private void checkClasspath() {
 		logger.debug("Checking classpath");
 		checkClasspath(ClassUtils.getClassname(Options.I().getMethodSignature()));
@@ -266,4 +292,72 @@ public class Generator {
 		return toReturn;
 	}
 	
+	private MethodDeclaration createSetResultsMethod(Type targetReturnType) {
+		MethodDeclaration set_results = new MethodDeclaration(Modifier.PUBLIC, ASTHelper.VOID_TYPE, "set_results");
+		List<Parameter> parameters = new ArrayList<Parameter>();
+		parameters.add(new Parameter(ASTHelper.createReferenceType(targetReturnType.toString(), 1), new VariableDeclaratorId("res")));
+		set_results.setParameters(parameters);
+		
+		List<Expression> init = new ArrayList<Expression>();
+		List<VariableDeclarator> decls = new ArrayList<VariableDeclarator>();
+		decls.add(new VariableDeclarator(new VariableDeclaratorId("i")));
+		init.add(new AssignExpr(new VariableDeclarationExpr(ASTHelper.INT_TYPE, decls), new IntegerLiteralExpr("0"), Operator.assign));
+		Expression condition = new BinaryExpr(ASTHelper.createNameExpr("i"), ASTHelper.createNameExpr(NUM_SCENARIOS), japa.parser.ast.expr.BinaryExpr.Operator.less);
+		List<Expression> increment = new ArrayList<Expression>();
+		increment.add(new UnaryExpr(ASTHelper.createNameExpr("i"), japa.parser.ast.expr.UnaryExpr.Operator.posIncrement));
+		BlockStmt forBody = new BlockStmt();
+		Expression left = new ArrayAccessExpr(ASTHelper.createNameExpr(ACTUAL_RESULT), ASTHelper.createNameExpr("i"));
+		Expression right = new ArrayAccessExpr(ASTHelper.createNameExpr("res"), ASTHelper.createNameExpr("i"));
+		AssignExpr assignment = new AssignExpr(left, right, Operator.assign);
+		ASTHelper.addStmt(forBody, assignment);
+		
+		ForStmt forStmt = new ForStmt(init,
+									condition, 
+									increment, 
+									forBody);
+		
+		BlockStmt methodBody = new BlockStmt();
+		ASTHelper.addStmt(methodBody, forStmt);
+		set_results.setBody(methodBody);
+		
+		return set_results;
+	}
+	
+	private MethodDeclaration createMethodUnderTest(Type targetReturnType) {
+		MethodDeclaration set_results = new MethodDeclaration(Modifier.PUBLIC, ASTHelper.VOID_TYPE, "method_under_test");
+		BlockStmt stmt = new BlockStmt();
+		
+		List<Expression> args = new ArrayList<Expression>();
+		args.add(new StringLiteralExpr("Executed"));
+		MethodCallExpr syso = new MethodCallExpr(new FieldAccessExpr(ASTHelper.createNameExpr("System"), "out"), "println", args);
+		
+		BinaryExpr condition = null;
+		for (int i = 0; i < 1; i++) { //FIXME: number of generated test scenarios	
+			Expression left_res = new ArrayAccessExpr(ASTHelper.createNameExpr(EXPECTED_RESULT), ASTHelper.createNameExpr(Integer.toString(i)));
+			Expression right_res = new ArrayAccessExpr(ASTHelper.createNameExpr(ACTUAL_RESULT), ASTHelper.createNameExpr(Integer.toString(i)));
+			BinaryExpr resCondition = new BinaryExpr(left_res, right_res, japa.parser.ast.expr.BinaryExpr.Operator.equals);
+			
+			List<Expression> distance_args = new ArrayList<Expression>();
+			distance_args.add(new ArrayAccessExpr(ASTHelper.createNameExpr(EXPECTED_STATE), ASTHelper.createNameExpr(Integer.toString(i))));
+			distance_args.add(new ArrayAccessExpr(ASTHelper.createNameExpr(ACTUAL_STATE), ASTHelper.createNameExpr(Integer.toString(i))));
+			
+			Expression left_state = new MethodCallExpr(new ThisExpr(), "distance", distance_args);
+			Expression right_state = new IntegerLiteralExpr("0");
+			BinaryExpr stateCondition = new BinaryExpr(left_state, right_state, japa.parser.ast.expr.BinaryExpr.Operator.equals);
+			BinaryExpr newCondition = new BinaryExpr(resCondition, stateCondition, japa.parser.ast.expr.BinaryExpr.Operator.and);
+			
+			if (condition != null) {
+				condition = new BinaryExpr(condition, newCondition, japa.parser.ast.expr.BinaryExpr.Operator.and);
+			}
+			else {
+				condition = newCondition;
+			}
+		}
+		
+		IfStmt ifStmt = new IfStmt(condition, new ExpressionStmt(syso), null);
+		ASTHelper.addStmt(stmt, ifStmt);
+		set_results.setBody(stmt);
+		
+		return set_results;
+	}
 }
