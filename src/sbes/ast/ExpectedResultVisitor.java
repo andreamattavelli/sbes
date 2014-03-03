423321c65f46407a95505675254bb5c068f6849d
Andrea Mattavelli <andreamattavelli@gmail.com>
2014-03-03 09:14:47 +0000

    Fixed bug when expected_result is composed of a cast expression
    git-svn-id: http://star.inf.usi.ch/repos/code/andrea/sbes@7612 62d94d92-d06f-44e7-bb87-548ab0999166

--- src/sbes/ast/ExpectedResultVisitor.java
+++ src/sbes/ast/ExpectedResultVisitor.java
@@ -6,6 +6,7 @@ import japa.parser.ASTHelper;
 import japa.parser.ast.body.VariableDeclarator;
 import japa.parser.ast.expr.ArrayAccessExpr;
 import japa.parser.ast.expr.AssignExpr;
+import japa.parser.ast.expr.CastExpr;
 import japa.parser.ast.expr.Expression;
 import japa.parser.ast.expr.MethodCallExpr;
 import japa.parser.ast.expr.VariableDeclarationExpr;
@@ -30,22 +31,32 @@ public class ExpectedResultVisitor extends VoidVisitorAdapter<String> {
 		VariableDeclarator vd = n.getVars().get(0);
 		if (vd.getInit() instanceof MethodCallExpr) {
 			MethodCallExpr mce = (MethodCallExpr) vd.getInit();
-			if (mce.getName().equals(methodName)) {
-				// found class constructor, switch to EXPECTED_STATES
-				Expression target = new ArrayAccessExpr(ASTHelper.createNameExpr(FirstStageStubGenerator.EXPECTED_RESULT),
-														ASTHelper.createNameExpr(Integer.toString(index)));
-				
-				if (mce.getScope() != null) {
-					expectedState = ASTUtils.getName(mce.getScope());
-					mce.setScope(new ArrayAccessExpr(ASTHelper.createNameExpr(FirstStageStubGenerator.EXPECTED_STATE),
-												ASTHelper.createNameExpr(Integer.toString(index))));
-				}
-
-				AssignExpr ae = new AssignExpr(target, mce, Operator.assign);
-				ExpressionStmt estmt = (ExpressionStmt) n.getParentNode();
-				estmt.setExpression(ae);
+			handleMethodCall(n, methodName, mce);
+		}
+		else if (vd.getInit() instanceof CastExpr) {
+			CastExpr ce = (CastExpr) vd.getInit();
+			if (ce.getExpr() instanceof MethodCallExpr) {
+				handleMethodCall(n, methodName, (MethodCallExpr) ce.getExpr());
 			}
 		}
 		super.visit(n, methodName);
+	}
+	
+	private void handleMethodCall(VariableDeclarationExpr n, String methodName, MethodCallExpr mce) {
+		if (mce.getName().equals(methodName)) {
+			// found class constructor, switch to EXPECTED_STATES
+			Expression target = new ArrayAccessExpr(ASTHelper.createNameExpr(FirstStageStubGenerator.EXPECTED_RESULT),
+													ASTHelper.createNameExpr(Integer.toString(index)));
+			
+			if (mce.getScope() != null) {
+				expectedState = ASTUtils.getName(mce.getScope());
+				mce.setScope(new ArrayAccessExpr(ASTHelper.createNameExpr(FirstStageStubGenerator.EXPECTED_STATE),
+											ASTHelper.createNameExpr(Integer.toString(index))));
+			}
+
+			AssignExpr ae = new AssignExpr(target, mce, Operator.assign);
+			ExpressionStmt estmt = (ExpressionStmt) n.getParentNode();
+			estmt.setExpression(ae);
+		}
 	}
 }
---
+++ test/sbes/stub/FirstStageStubGeneratorTest.java
@@ -0,0 +1,119 @@
+package sbes.stub;
+
+import static org.junit.Assert.*;
+import japa.parser.JavaParser;
+import japa.parser.ParseException;
+import japa.parser.ast.ImportDeclaration;
+import japa.parser.ast.stmt.BlockStmt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import sbes.option.Options;
+import sbes.scenario.GenericTestScenario;
+import sbes.scenario.TestScenario;
+import sbes.scenario.TestScenarioGeneralizer;
+import sbes.testcase.CarvingResult;
+
+public class FirstStageStubGeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws ParseException {
+		List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
+		
+		Options.I().setClassesPath("/Users/andrea/Uni/PhD/Workspaces/sbes-synthesis/sbes/joda-time-2.3.jar");
+		Options.I().setMethodSignature("org.joda.time.DateTime.minus(ReadableDuration)");
+		
+		BlockStmt body = JavaParser.parseBlock("{FixedDateTimeZone fixedDateTimeZone0 = new FixedDateTimeZone(\"\", \"\", 1836, 0);"+ 
+				"DateTime dateTime0 = new DateTime((DateTimeZone) fixedDateTimeZone0);"+ 
+				"Interval interval0 = new Interval((long) (-64), (long) 1836);"+ 
+				"Duration duration0 = interval0.toDuration();"+ 
+				"DateTime dateTime1 = dateTime0.minus((ReadableDuration) duration0);}");
+		CarvingResult cr = new CarvingResult(body, imports);
+		
+		TestScenarioGeneralizer tsg = new TestScenarioGeneralizer(0);
+		TestScenario ts = tsg.generalizeTestToScenario(cr);
+		
+		System.out.println(ts.getScenario().toString());
+		System.out.println(ts.getInputs());
+	}
+	
+	@Test
+	public void test2() throws ParseException {
+		List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
+		
+		Options.I().setClassesPath("/Users/andrea/Uni/PhD/Workspaces/sbes-synthesis/Stack-UseCase/bin");
+		Options.I().setMethodSignature("stack.util.Stack.push(Object)");
+		
+		BlockStmt body = JavaParser.parseBlock("{Stack<Integer> stack0 = new Stack<Integer>();"+
+				"Integer integer0 = new Integer(954);"+
+				"Integer integer1 = stack0.push(integer0);}");
+		CarvingResult cr = new CarvingResult(body, imports);
+		
+		TestScenarioGeneralizer tsg = new TestScenarioGeneralizer(0);
+		TestScenario ts = tsg.generalizeTestToScenario(cr);
+		
+		System.out.println(ts.getScenario().toString());
+		System.out.println(ts.getInputs());
+		
+		assertEquals(GenericTestScenario.class, ts.getClass());
+	}
+
+	@Test
+	public void test3() throws ParseException {
+		List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
+		
+		Options.I().setClassesPath("/Users/andrea/Uni/PhD/Workspaces/sbes-synthesis/sbes/joda-time-2.3.jar");
+		Options.I().setMethodSignature("org.joda.time.DateTime.minus(ReadableDuration)");
+		
+		BlockStmt body = JavaParser.parseBlock("{BuddhistChronology buddhistChronology0 = BuddhistChronology.getInstance();"+
+				"CachedDateTimeZone cachedDateTimeZone0 = (CachedDateTimeZone)buddhistChronology0.getZone();"+
+				"DateTime dateTime0 = DateTime.now((DateTimeZone) cachedDateTimeZone0);"+
+				"PeriodType periodType0 = PeriodType.hours();"+
+				"MutablePeriod mutablePeriod0 = new MutablePeriod((Object) null, periodType0);"+
+				"Duration duration0 = mutablePeriod0.toDurationFrom((ReadableInstant) dateTime0);"+
+				"DateTime dateTime1 = dateTime0.minus((ReadableDuration) duration0);}");
+		CarvingResult cr = new CarvingResult(body, imports);
+		
+		TestScenarioGeneralizer tsg = new TestScenarioGeneralizer(0);
+		TestScenario ts = tsg.generalizeTestToScenario(cr);
+		
+		System.out.println(ts.getScenario().toString());
+		
+		System.out.println(ts.getInputs());
+	}
+	
+	@Test
+	public void test4() throws ParseException {
+		List<ImportDeclaration> imports = new ArrayList<ImportDeclaration>();
+		
+		Options.I().setClassesPath("/Users/andrea/Uni/PhD/Workspaces/sbes-synthesis/sbes/gs-core-1.2.jar");
+		Options.I().setMethodSignature("org.graphstream.graph.implementations.AbstractEdge.getNode0()");
+		
+		BlockStmt body = JavaParser.parseBlock("{AdjacencyListGraph adjacencyListGraph0 = new AdjacencyListGraph(\"\");"+
+												"MultiNode multiNode0 = new MultiNode((AbstractGraph) adjacencyListGraph0, \"\");"+
+												"SingleNode singleNode0 = new SingleNode(adjacencyListGraph0, \"ele\");"+
+												"AbstractEdge abstractEdge0 = new AbstractEdge(\"\", singleNode0, multiNode0, false);"+
+												"SingleNode singleNode1 = (SingleNode)abstractEdge0.getNode0();}");
+		CarvingResult cr = new CarvingResult(body, imports);
+		
+		TestScenarioGeneralizer tsg = new TestScenarioGeneralizer(0);
+		TestScenario ts = tsg.generalizeTestToScenario(cr);
+		
+		System.out.println(ts.getScenario().toString());
+		System.out.println(ts.getInputs());
+	}
+	
+}
