7c812f25db581df402e8572ed9fe119d2215aa91
Andrea Mattavelli <andreamattavelli@gmail.com>
2014-03-05 16:42:28 +0000

    Improved support for generics
    git-svn-id: http://star.inf.usi.ch/repos/code/andrea/sbes@7676 62d94d92-d06f-44e7-bb87-548ab0999166

--- src/sbes/stub/generator/SecondStageGenericStubGenerator.java
+++ src/sbes/stub/generator/SecondStageGenericStubGenerator.java
@@ -1,24 +1,15 @@
 package sbes.stub.generator;
 
-import japa.parser.ASTHelper;
-import japa.parser.ast.body.VariableDeclarator;
-import japa.parser.ast.body.VariableDeclaratorId;
-import japa.parser.ast.expr.AssignExpr;
-import japa.parser.ast.expr.Expression;
-import japa.parser.ast.expr.MethodCallExpr;
-import japa.parser.ast.expr.ThisExpr;
-import japa.parser.ast.expr.VariableDeclarationExpr;
-import japa.parser.ast.expr.AssignExpr.Operator;
-import japa.parser.ast.stmt.ExpressionStmt;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.TypeDeclaration;
+import japa.parser.ast.type.ClassOrInterfaceType;
 
-import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
-import sbes.option.Options;
 import sbes.result.CarvingResult;
 import sbes.stub.Stub;
-import sbes.util.ClassUtils;
 
 public class SecondStageGenericStubGenerator extends SecondStageStubGenerator {
 
@@ -30,16 +21,19 @@ public class SecondStageGenericStubGenerator extends SecondStageStubGenerator {
 	}
 	
 	@Override
-	protected ExpressionStmt createCloneObj(Method targetMethod) {
-		List<Expression> methodParameters = new ArrayList<Expression>();
-		methodParameters.add(new ThisExpr());
-		Expression right = new MethodCallExpr(ASTHelper.createNameExpr("c"), "deepClone", methodParameters);
-		List<VariableDeclarator> vars = new ArrayList<VariableDeclarator>();
-		vars.add(new VariableDeclarator(new VariableDeclaratorId("clone")));
-		String className = ClassUtils.getSimpleClassname(Options.I().getMethodSignature()) + "<" + concreteClass + ">";
-		Expression left = new VariableDeclarationExpr(ASTHelper.createReferenceType(className, 0), vars);
-		AssignExpr assignment = new AssignExpr(left, right, Operator.assign);
-		return new ExpressionStmt(assignment);
+	protected TypeDeclaration getClassDeclaration(String className) {
+		stubName = className + STUB_EXTENSION + "_2";
+		String genericDecl = "<E extends " + concreteClass + ">";
+		
+		// extends base class
+		ClassOrInterfaceType extendClassDecl = new ClassOrInterfaceType(className + "<E>");
+		List<ClassOrInterfaceType> extendClasses = new ArrayList<ClassOrInterfaceType>();
+		extendClasses.add(extendClassDecl);
+		
+		ClassOrInterfaceDeclaration classDecl = new ClassOrInterfaceDeclaration(Modifier.PUBLIC, false, stubName + genericDecl);
+		classDecl.setExtends(extendClasses);
+		
+		return classDecl;
 	}
 
 }
