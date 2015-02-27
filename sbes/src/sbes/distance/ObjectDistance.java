92062ad2da9ff7ed11814b09a2c1840c87fc858e
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-02-27 13:42:17 +0000

    Updated distance

--- sbes/src/sbes/distance/ObjectDistance.java
+++ sbes/src/sbes/distance/ObjectDistance.java
@@ -1,5 +1,9 @@
 package sbes.distance;
 
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
+
 import sbes.util.ReflectionUtils;
 
 public class ObjectDistance {
@@ -10,7 +14,7 @@ public class ObjectDistance {
 				return 1.0 * Distance.NULL_WEIGHT;
 			}
 			else {
-				int size = ReflectionUtils.getInheritedPrivateFields(o2.getClass()).size();
+				int size = getObjectSize(o2);
 				return size > 0 ? size * Distance.NULL_WEIGHT : Distance.NULL_WEIGHT;
 			}
 		}
@@ -19,10 +23,31 @@ public class ObjectDistance {
 				return 1.0 * Distance.NULL_WEIGHT;
 			}
 			else {
-				int size = ReflectionUtils.getInheritedPrivateFields(o1.getClass()).size();
+				int size = getObjectSize(o1);
 				return size > 0 ? size * Distance.NULL_WEIGHT : Distance.NULL_WEIGHT;
 			}
 		}
+	}
+	
+	private static int getObjectSize(Object o) {
+		int size = 0;
+		List<Class<?>> worklist = new LinkedList<Class<?>>();
+		
+		worklist.add(o.getClass());
+		while (!worklist.isEmpty()) {
+			Class<?> c = worklist.remove(0);
+			List<Field> fields = ReflectionUtils.getInheritedPrivateFields(c);
+			size = fields.size();
+			for (Field field : fields) {
+				Class<?> fieldClass = field.getType();
+				size += ReflectionUtils.getInheritedPrivateFields(fieldClass).size();
+				if (!fieldClass.equals(c) && !worklist.contains(fieldClass)) {
+					worklist.add(field.getType());
+				}
+			}
+		}
+				
+		return size;
 	}
 	
 }
