00d3bf790f9f51939aff97933f2b2324aff6ea6a
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-03-11 14:41:08 +0000

    Refactored and updated test cases

--- sbes/test/sbes/stub/SecondStageStubGeneratorTest.java
+++ sbes/test/sbes/stub/SecondStageStubGeneratorTest.java
@@ -77,19 +77,19 @@ public class SecondStageStubGeneratorTest {
 		try {
 			Path directory = Paths.get("./test/resources/compilation");
 			Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
-				   @Override
-				   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					   Files.delete(file);
-					   return FileVisitResult.CONTINUE;
-				   }
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.delete(file);
+					return FileVisitResult.CONTINUE;
+				}
 
-				   @Override
-				   public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-					   Files.delete(dir);
-					   return FileVisitResult.CONTINUE;
-				   }
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
 
-			   });
+			});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
