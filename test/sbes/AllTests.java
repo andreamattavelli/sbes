52e08c21f70f44d5b4cae7ddae17535b3401ac20
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-03-11 15:33:50 +0000

    Refactored and updated test cases
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES/trunk/sbes@8929 62d94d92-d06f-44e7-bb87-548ab0999166

--- test/sbes/AllTests.java
+++ test/sbes/AllTests.java
@@ -5,11 +5,15 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import sbes.distance.DistanceTest;
+import sbes.scenario.CounterexampleGeneralizerTest;
 import sbes.stub.FirstStageStubGeneratorTest;
 import sbes.stub.SecondStageStubGeneratorTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({DistanceTest.class, FirstStageStubGeneratorTest.class, SecondStageStubGeneratorTest.class})
+@SuiteClasses({	DistanceTest.class,
+				CounterexampleGeneralizerTest.class,
+				FirstStageStubGeneratorTest.class, 
+				SecondStageStubGeneratorTest.class})
 public class AllTests {
 
 }
