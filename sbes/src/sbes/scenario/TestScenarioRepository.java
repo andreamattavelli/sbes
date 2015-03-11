d0a72573d6d3ffeb2945bfea30e1a87e43f7af17
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-03-11 09:30:12 +0000

    Fixed bug in iterations: we now remove the counterexamples generated during previous iterations

---
+++ sbes/src/sbes/scenario/TestScenarioRepository.java
@@ -0,0 +1,48 @@
+package sbes.scenario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestScenarioRepository {
+
+	private static TestScenarioRepository instance;
+
+	private List<TestScenario> scenarios;
+	private List<TestScenario> counterexamples;
+
+	private TestScenarioRepository() {
+		scenarios = new ArrayList<TestScenario>();
+		counterexamples = new ArrayList<TestScenario>();
+	}
+
+	public static TestScenarioRepository I() {
+		if (instance == null) {
+			instance = new TestScenarioRepository();
+		}
+		return instance;
+	}
+	
+	public void resetCounterexamples() {
+		counterexamples.clear();
+	}
+	
+	public List<TestScenario> getScenarios() {
+		ArrayList<TestScenario> all = new ArrayList<>();
+		all.addAll(scenarios);
+		all.addAll(counterexamples);
+		return all;
+	}
+
+	public void addScenario(TestScenario testScenario) {
+		scenarios.add(testScenario);
+	}
+	
+	public void addScenarios(List<TestScenario> initialScenarios) {
+		scenarios.addAll(initialScenarios);
+	}
+	
+	public void addCounterexample(TestScenario counterexampleScenario) {
+		counterexamples.add(counterexampleScenario);
+	}
+	
+}
