759ac4b9a5386f2e73487a4ffdd1cc3d620ff7fa
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-03-17 11:12:57 +0000

    Modified mapping of generic <-> concrete classes. Still working on it Fixed some tests
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES/trunk/sbes@8997 62d94d92-d06f-44e7-bb87-548ab0999166

--- test/sbes/stub/generator/first/FirstStageGeneratorFactoryTest.java
+++ test/sbes/stub/generator/first/FirstStageGeneratorFactoryTest.java
@@ -6,9 +6,13 @@ import japa.parser.ast.ImportDeclaration;
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.stmt.BlockStmt;
 
+import java.lang.reflect.GenericDeclaration;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -16,8 +20,9 @@ import org.junit.Test;
 import sbes.logging.Level;
 import sbes.option.Options;
 import sbes.result.CarvingResult;
-import sbes.scenario.TestScenarioWithGenerics;
 import sbes.scenario.TestScenario;
+import sbes.scenario.TestScenarioWithGenerics;
+import sun.reflect.generics.reflectiveObjects.TypeVariableImpl;
 
 public class FirstStageGeneratorFactoryTest {
 
@@ -41,10 +46,11 @@ public class FirstStageGeneratorFactoryTest {
 		BlockStmt blockStmt0 = new BlockStmt();
 		CarvingResult carvingResult0 = new CarvingResult(blockStmt0, (List<ImportDeclaration>) null);
 		LinkedList<FieldDeclaration> linkedList1 = new LinkedList<FieldDeclaration>();
-		LinkedList<String> linkedList2 = new LinkedList<String>();
-		linkedList2.add("Integer");
+		Map<TypeVariable<?>, String> genericToConcrete = new LinkedHashMap<>();
+		TypeVariable<?> tv = TypeVariableImpl.<GenericDeclaration>make(Object.class, "E", null, null);
+		genericToConcrete.put(tv, "Integer");
 		TestScenario testScenario0 = new TestScenario(carvingResult0, blockStmt0, (List<FieldDeclaration>) linkedList1);
-		linkedList0.add(new TestScenarioWithGenerics((CarvingResult) testScenario0, blockStmt0, linkedList1, linkedList2));
+		linkedList0.add(new TestScenarioWithGenerics((CarvingResult) testScenario0, blockStmt0, linkedList1, genericToConcrete));
 		FirstStageGeneratorStub firstStageGeneratorStubWithGenerics0 = FirstStageGeneratorFactory.createGenerator((List<TestScenario>) linkedList0);
 		assertNotNull(firstStageGeneratorStubWithGenerics0);
 		assertEquals(FirstStageGeneratorStubWithGenerics.class, firstStageGeneratorStubWithGenerics0.getClass());
