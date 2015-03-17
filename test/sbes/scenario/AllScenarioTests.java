4dbb932435f01cae8e1ef88d827e3cf59ada6b3b
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-03-17 11:05:27 +0000

    Added test suites
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES/trunk/sbes@8994 62d94d92-d06f-44e7-bb87-548ab0999166

---
+++ test/sbes/scenario/AllScenarioTests.java
@@ -0,0 +1,12 @@
+package sbes.scenario;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CounterexampleGeneralizerTest.class,
+		TestScenarioGeneralizerTest.class, TestScenarioLoaderTest.class })
+public class AllScenarioTests {
+
+}
