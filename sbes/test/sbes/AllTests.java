e47b3412a826cb2dca4d2cc51e549fc9e040b68b
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-04-07 07:53:42 +0000

    Refactored tests

--- sbes/test/sbes/AllTests.java
+++ sbes/test/sbes/AllTests.java
@@ -7,12 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import sbes.distance.AllDistanceTests;
 import sbes.scenario.AllScenarioTests;
 import sbes.stub.generator.AllGeneratorTests;
+import sbes.util.AllUtilsTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	AllDistanceTests.class,
 	AllScenarioTests.class,
-	AllGeneratorTests.class
+	AllGeneratorTests.class,
+	AllUtilsTests.class
 })
 public class AllTests {
 
