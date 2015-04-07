f61a48234355dea13b54166b73f20edc8f2200af
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-04-07 07:53:42 +0000

    Refactored tests
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES/trunk/sbes@9082 62d94d92-d06f-44e7-bb87-548ab0999166

---
+++ test/sbes/util/AllUtilsTests.java
@@ -0,0 +1,11 @@
+package sbes.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ReflectionUtilsTest.class })
+public class AllUtilsTests {
+
+}
