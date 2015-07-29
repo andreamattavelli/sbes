406eb8ea089bac9850ac97f7cc75de7afe99b2c2
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-07-29 13:20:32 +0000

    Improvements and fixes

--- sbes/src/sbes/exceptions/CloningException.java
+++ sbes/src/sbes/exceptions/CloningException.java
@@ -1,12 +1,5 @@
 package sbes.exceptions;
 
-/**
- * thrown if cloning fails
- * 
- * @author kostantinos.kougios
- *
- * 18 Jan 2009
- */
 public class CloningException extends RuntimeException {
 
     private static final long serialVersionUID = 3815175312001146867L;
@@ -14,4 +7,5 @@ public class CloningException extends RuntimeException {
     public CloningException(final String message, final Throwable cause) {
         super(message, cause);
     }
+    
 }
