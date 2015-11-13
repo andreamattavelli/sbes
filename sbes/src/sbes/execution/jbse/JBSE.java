3dbd88e3fe97e71a8b109456561edb50450c49ae
Andrea Mattavelli <andreamattavelli@gmail.com>
2015-11-13 10:41:47 +0000

    Bugfix

--- sbes/src/sbes/execution/jbse/JBSE.java
+++ sbes/src/sbes/execution/jbse/JBSE.java
@@ -73,7 +73,7 @@ public class JBSE extends Tool {
 
 		// scope
 		p.setTimeout(Options.I().getCounterexampleBudget(), TimeUnit.SECONDS);
-		p.setHeapScope("doubly_linked_list/DoubleLinkedList_LICS$Entry", 8);
+		p.setHeapScope("sbes/symbolic/mock/DoubleLinkedList$Entry", 8);
 		p.setDepthScope(500);
 		p.setCountScope(2000);
 		p.setConcretizationDepthScope(1000);
